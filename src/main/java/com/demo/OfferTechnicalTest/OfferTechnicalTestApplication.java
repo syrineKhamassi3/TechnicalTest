993d193c3da9f61ec031c86710082b4757cfbe9e
SyrineKHAMASSI <syrine.khamassi@teamwill-group.com>
2021-10-31 12:01:29 +0100

    initial commit

---
+++ src/main/java/com/demo/OfferTechnicalTest/OfferTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.demo.OfferTechnicalTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfferTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/OfferTechnicalTest/OfferTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.OfferTechnicalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
